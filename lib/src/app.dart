8d5d3a45ba5305c3719622f2f775a39e8b087559
Nathan Queija <nathanqcam@gmail.com>
2018-06-29 00:02:11 -0300

    Use Stateless widget

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,3 +1,21 @@
 // Import flutter helper library
+import 'package:flutter/material.dart';
 
 // Create a class that will be our custom widget
+// must extend the Stateless widget
+class App extends StatelessWidget {
+  //must define a build method that return a widget that this widget will show
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Lets see some images!'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => print('Hi there'),
+          child: Icon(Icons.add_a_photo),
+        ),
+      ),
+    );
+  }
+}
