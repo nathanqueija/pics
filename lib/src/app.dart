84334185a663ee3fecd4eb4d5daf2b881906a3d6
Nathan Queija <nathanqcam@gmail.com>
2018-06-29 17:39:12 -0300

    Add ImageList widget

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,9 +1,34 @@
 // Import flutter helper library
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'widgets/image_list.dart';
+
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
 
 // Create a class that will be our custom widget
 // must extend the Stateless widget
-class App extends StatelessWidget {
+class AppState extends State<App> {
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    final response =
+        await get('https://jsonplaceholder.typicode.com/photos/$counter');
+
+    final imageModel = ImageModel.fromJson(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
   //must define a build method that return a widget that this widget will show
   Widget build(context) {
     return MaterialApp(
@@ -12,9 +37,10 @@ class App extends StatelessWidget {
           title: Text('Lets see some images!'),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () => print('Hi there'),
+          onPressed: fetchImage,
           child: Icon(Icons.add_a_photo),
         ),
+        body: ImageList(images),
       ),
     );
   }
---
+++ lib/src/widgets/image_list.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import '../models/image_model.dart';
+
+class ImageList extends StatelessWidget {
+  final List<ImageModel> images;
+
+  ImageList(this.images);
+  Widget build(BuildContext context) {}
+}
