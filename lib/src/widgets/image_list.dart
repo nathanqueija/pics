d2b61f2b488333d87f4f66ff20787c5d7b57fac0
Nathan Queija <nathanqcam@gmail.com>
2018-06-30 00:35:47 -0300

    Add Image title and url

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -5,5 +5,28 @@ class ImageList extends StatelessWidget {
   final List<ImageModel> images;
 
   ImageList(this.images);
-  Widget build(BuildContext context) {}
+
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: images.length,
+      itemBuilder: (context, int index) {
+        return buildImage(images[index]);
+      },
+    );
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          Image.network(image.url),
+          Text(image.title),
+          Text(image.url),
+        ],
+      ),
+      decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+    );
+  }
 }
